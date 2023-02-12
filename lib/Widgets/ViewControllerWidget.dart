de4826de605711e25c641afa22105bb729eed04f
Ajay <ajayyasodha@gmail.com>
2023-02-12 15:48:20 +0530

    feat: [GoFlutter-2] Added static HomeAppBar and Placeholder Profile AppBar.

--- lib/Widgets/ViewControllerWidget.dart
+++ lib/Widgets/ViewControllerWidget.dart
@@ -1,7 +1,9 @@
 // Package Dependencies
+import 'package:flutter/material.dart';
+
+// Widgets
 import 'package:first_app/Widgets/Screens/HomeWidget.dart';
 import 'package:first_app/Widgets/Screens/ProfileWidget.dart';
-import 'package:flutter/material.dart';
 
 class ViewControllerWidget extends StatefulWidget {
   const ViewControllerWidget({super.key});
@@ -16,25 +18,83 @@ class _ViewControllerWidgetState extends State<ViewControllerWidget> {
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      presentingWidget(_selectedIndex);
+      presentingBodyWidget(_selectedIndex);
     });
   }
 
-  Widget presentingWidget(int index) {
+  PreferredSizeWidget? presentingAppBarWidget(int index) {
+    switch (index) {
+      case 0:
+        return AppBar(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "It All Starts Here",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: const [
+                  Text(
+                    "Chennai",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Icon(Icons.chevron_right, color: Colors.grey, size: 14),
+                ],
+              ),
+            ],
+          ),
+          backgroundColor: const Color.fromRGBO(28, 46, 74, 0.9),
+          actions: const [
+            Icon(
+              Icons.search,
+            ),
+            SizedBox(
+              width: 12,
+            ),
+            Icon(Icons.notifications_outlined),
+            SizedBox(
+              width: 12,
+            ),
+          ],
+          toolbarHeight: 72,
+        );
+      case 1:
+        return AppBar(
+          backgroundColor: const Color.fromRGBO(28, 46, 74, 0.9),
+          toolbarHeight: 72,
+        );
+    }
+    return null;
+  }
+
+  Widget? presentingBodyWidget(int index) {
     switch (index) {
       case 0:
         return const HomeWidget();
       case 1:
         return const ProfileWidget();
-      default:
-        return Container();
     }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: presentingWidget(_selectedIndex),
+      appBar: presentingAppBarWidget(_selectedIndex),
+      body: presentingBodyWidget(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.red[400],
         unselectedItemColor: Colors.grey,
