2c30914b86a7cfa89301d7fb6284f689ea00c76d
Ajay <ajayyasodha@gmail.com>
2023-02-16 22:20:48 +0530

    chore: [GoFlutter-1] Added NumberFormatter Utility along with Test Suite.

---
+++ lib/Resources/Utitlity/number_formatter.dart
@@ -0,0 +1,6 @@
+// Package Dependencies
+import 'package:intl/intl.dart';
+
+class NumberFormatter {
+  static String format(_) => NumberFormat.compact().format(_);
+}
---
+++ test/resources/number_formatter_test.dart
@@ -0,0 +1,33 @@
+// Package Dependencies
+import 'package:flutter_test/flutter_test.dart';
+
+// Resources
+import 'package:first_app/resources/Utitlity/number_formatter.dart';
+
+void main() {
+  group('Number Formatting', () {
+    test('4-digit suffix K', () {
+      var result = NumberFormatter.format(1200);
+      expect(result, '1.2K');
+    });
+    test('5-digit suffix K', () {
+      var result = NumberFormatter.format(12345);
+      expect(result, '12.3K');
+    });
+
+    test('6-digit suffix K', () {
+      var result = NumberFormatter.format(987654);
+      expect(result, '988K');
+    });
+
+    test('8-digit suffix M', () {
+      var result = NumberFormatter.format(85989543);
+      expect(result, '86M');
+    });
+
+    test('11-digit suffix B', () {
+      var result = NumberFormatter.format(42825671290);
+      expect(result, '42.8B');
+    });
+  });
+}
