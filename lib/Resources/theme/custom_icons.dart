425a896af4ae133ed62440b8c62b63b7da08331d
Ajay <ajayyasodha@gmail.com>
2023-02-18 13:13:57 +0530

    chore: [GoFlutter-3] Added custom icons.

---
+++ lib/Resources/theme/custom_icons.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData saved =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData notification =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData setting =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData ticket =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
